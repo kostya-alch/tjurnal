c2ddbbae71fcad031f82dc7cd2d9c3c8cdc472f7
kostya-alch <kostafrompiter@gmail.com>
2022-09-10 00:06:20 +0300

    finish comments page

---
+++ frontend/components/Comment/index.tsx
@@ -0,0 +1,54 @@
+import styles from './Comment.module.scss';
+import { IconButton, Menu, MenuItem, Typography } from '@material-ui/core';
+import { FC, useState } from 'react';
+import { MoreHoriz as MoreIcon } from '@material-ui/icons';
+
+interface CommentProps {
+  user: {
+    fullName: string;
+  };
+  text: string;
+}
+
+export const Comment: FC<CommentProps> = ({ text, user }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={styles.comment}>
+      <div className={styles.userInfo}>
+        <img
+          src="https://leonardo.osnova.io/2d20257c-fec5-4b3e-7f60-055c86f24a4d/-/scale_crop/108x108/-/format/webp/"
+          alt="Avatar"
+        />
+        <b>Logan Poul</b>
+        <span>5 часов</span>
+      </div>
+      <Typography className={styles.text}>
+        По словам Рогова, ВСУ пытается прощупать оборону и ищет место, где подразделения могут
+        закрепиться на берегу и создать плацдарм для наступления.
+      </Typography>
+      <span className={styles.replyBtn}>Ответить</span>
+      <IconButton onClick={handleClick}>
+        <MoreIcon />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        elevation={2}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        keepMounted
+      >
+        <MenuItem onClick={handleClose}>Удалить</MenuItem>
+        <MenuItem onClick={handleClose}>Редактировать</MenuItem>
+      </Menu>
+    </div>
+  );
+};
--- frontend/pages/news/[slug].tsx
+++ frontend/pages/news/[slug].tsx
@@ -1,11 +1,22 @@
 import { MainLayout } from '../../layout/MainLayout';
-import { Paper, Typography } from '@material-ui/core';
+import { Divider, Paper, Tab, Tabs, Typography } from '@material-ui/core';
 import { FullPost } from '../../components/FullPost';
+import { Comment } from '../../components/Comment';
 
 export default function News() {
   return (
-    <MainLayout contentFullWidth>
+    <MainLayout contentFullWidth className={'mb-50'}>
       <FullPost />
+      <Paper elevation={0} className={'mt-40 p-30'}>
+        <Typography variant={'h6'}>42 комментария</Typography>
+        <Tabs className="mt-20" value={0} indicatorColor="primary" textColor="primary">
+          <Tab label="Популярные" />
+          <Tab label="По порядку" />
+        </Tabs>
+        <Divider />
+        <div className={'mb-20'} />
+        <Comment />
+      </Paper>
     </MainLayout>
   );
 }
