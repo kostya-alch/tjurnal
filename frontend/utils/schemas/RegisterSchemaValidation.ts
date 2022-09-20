edcb296f42f080fbe00ce62ce1a6bec6056c836d
kostya-alch <kostafrompiter@gmail.com>
2022-09-21 02:29:25 +0300

    refactor register validation

--- frontend/utils/schemas/RegisterSchemaValidation.ts
+++ frontend/utils/schemas/RegisterSchemaValidation.ts
@@ -1,10 +1,9 @@
 import * as yup from 'yup';
+import { LoginSchema } from './loginSchemaValidation';
 
-export const RegisterSchema = yup.object().shape({
-  fullname: yup.string().min(2, 'Введите имя и фамилию').required('Имя и фамилия обязательны'),
-  email: yup.string().email('Неверная почта').required('Почта обязательная'),
-  password: yup
-    .string()
-    .min(6, 'Пароль должен быть не менее 6 символов')
-    .required('Пароль обязательный'),
-});
+export const RegisterSchema = yup
+  .object()
+  .shape({
+    fullname: yup.string().min(2, 'Введите имя и фамилию').required('Имя и фамилия обязательны'),
+  })
+  .concat(LoginSchema);
